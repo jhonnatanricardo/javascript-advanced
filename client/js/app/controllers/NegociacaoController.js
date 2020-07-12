86af6d8024cb8eccef8c20f886fe6d2aa4201581
Jhonnatan Ricardo Semler <jhonnatanricardo@gmail.com>
2020-07-12 20:00:30 -0300

    Lesson 3

---
+++ client/js/app/controllers/NegociacaoController.js
@@ -0,0 +1,19 @@
+class NegociacaoController {
+
+    constructor(){
+        let $ = document.querySelector.bind(document);
+        this._inputData = $("#data");
+        this._inputQuantidade = $("#quantidade");
+        this._inputValor = $("#valor");
+    }
+
+    adiciona(event){
+        event.precentDefault();
+
+        let data = new Date(...this._inputData.value
+                                        .split("-")
+                                        .map((item, indice) => item - (indice % 2)));
+
+        let negociacao = new Negociacao(data, this._inputQuantidade.value, this._inputValor.value);
+    }
+}
