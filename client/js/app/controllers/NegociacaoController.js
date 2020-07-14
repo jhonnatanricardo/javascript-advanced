46bae3c116f3f9e194b148bcacbf4ce206b246bc
Jhonnatan Ricardo Semler <jhonnatanricardo@gmail.com>
2020-07-13 23:50:17 -0300

    Lesson 5

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,17 +6,21 @@ class NegociacaoController {
         this._inputQuantidade = $("#quantidade");
         this._inputValor = $("#valor");
         this._listaNegociacoes = new ListaNegociacoes();
+        this._negociacoesView = new NegociacoesView($("#negociacoesView"));
+
+        this._negociacoesView.update(this._listaNegociacoes);
     }
 
     adiciona(event){
         event.preventDefault();
         let negociacao = this._criaNegociacao();
         this._listaNegociacoes.adiciona(negociacao);
+        this._negociacoesView.update(this._listaNegociacoes);
         this._limpaFormulario();
     }
 
     _criaNegociacao(){
-        return negociacao = new Negociacao(DateHelper.textToDate(this._inputData.value), this._inputQuantidade.value, this._inputValor.value);
+        return new Negociacao(DateHelper.textToDate(this._inputData.value), this._inputQuantidade.value, this._inputValor.value);
     }
 
     _limpaFormulario () {
---
+++ client/js/app/views/NegociacoesView.js
@@ -0,0 +1,43 @@
+class NegociacoesView {
+
+    constructor(elemento){
+        this._elemento = elemento;
+    }
+
+    _template(model) {
+        return `<table class="table table-hover table-bordered">
+                    <thead>
+                        <tr>
+                            <th>DATA</th>
+                            <th>QUANTIDADE</th>
+                            <th>VALOR</th>
+                            <th>VOLUME</th>
+                        </tr>
+                    </thead>
+                    
+                    <tbody>
+                    </tbody>
+
+                        ${model.negociacoes.map((negociacao)=>
+                            `
+                                <tr>
+                                    <td>${DateHelper.dateToText(negociacao.data)}</td>
+                                    <td>${negociacao.quantidade}</td>
+                                    <td>${negociacao.valor}</td>
+                                    <td>${negociacao.volume}</td>
+                                </tr>
+                            `
+                        ).join('')}
+                    
+                    <tfoot>
+                        <td colspan="3"></td>
+                        <td>${model.negociacoes.reduce((total, n) => total += n.volume, 0.0)}
+                        </td>
+                    </tfoot>
+                </table>`;
+    }
+
+    update(listaNegociacoes) {
+        this._elemento.innerHTML = this._template(listaNegociacoes);
+    }
+}
