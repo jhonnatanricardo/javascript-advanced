40a56472499306004dd819dbfe240a64af26e329
Jhonnatan Ricardo Semler <jhonnatanricardo@gmail.com>
2020-07-15 00:26:22 -0300

    Lesson 4, Part 2

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -28,6 +28,24 @@ class NegociacaoController {
         this._limpaFormulario();
     }
 
+    importaNegociacoes(){
+        
+        let service = new NegociacaoService();
+
+        service.obterNegociacoesDaSemana((erro, negociacoes) => {
+            
+            if(erro){
+                this._mensagem.texto = erro;
+                return;
+            }
+
+            negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+            this._mensagem.texto = "Negociações importadas com sucesso";
+
+        });
+
+    }
+
     apaga(){
         this._mensagem.texto = "Negociações apagadas com sucesso";
         this._listaNegociacoes.esvazia();
