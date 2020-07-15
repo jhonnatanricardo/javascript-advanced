90ab1475ea492e494ddf09921178694d67ffdf65
Jhonnatan Ricardo Semler <jhonnatanricardo@gmail.com>
2020-07-14 23:56:38 -0300

    Lesson 3, part 2

--- client/js/app/models/ListaNegociacoes.js
+++ client/js/app/models/ListaNegociacoes.js
@@ -1,12 +1,10 @@
 class ListaNegociacoes {
-    constructor(armadilha){
+    constructor(){
         this._negociacoes = [];
-        this._armadilha = armadilha;
     }
 
     adiciona(negociacao){
         this._negociacoes.push(negociacao);
-        this._armadilha(this);
     }
 
     /**
@@ -18,6 +16,5 @@ class ListaNegociacoes {
 
     esvazia() {
         this._negociacoes = [];
-        this._armadilha(this);
     }
 }
